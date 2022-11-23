b3cb3fca84e25fc7d6c06d0ad6700c61991c16b9
Bartosz Owczarzak <barteksbr@gmail.com>
2022-11-23 17:01:16 +0100

    js edited

--- js/script.js
+++ js/script.js
@@ -1,28 +1,49 @@
-let formElement = document.querySelector(".js-form");
-let plnElement = document.querySelector(".js-pln");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+{
 
-let USD = 4.50;
-let AUD = 3.05;
-let EUR = 4.70;
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+    const calculateResult = (pln, currency) => {
 
-    let pln = plnElement.value;
-    let currency = currencyElement.value;
+        const USD = 4.50;
+        const AUD = 3.05;
+        const EUR = 4.70;
 
-    switch (currency) {
-        case "EUR":
-            result = pln / EUR;
-            break;
-        case "USD":
-            result = pln / USD;
-            break;
-        case "AUD":
-            result = pln / AUD;
-            break;
-    }
-    resultElement.innerText = `${result.toFixed(2)} ${currency}`
-})
+        switch (currency) {
+            case "EUR":
+                return pln / EUR;
+
+            case "USD":
+                return pln / USD;
+
+            case "AUD":
+                return pln / AUD;
+
+        }
+    };
+
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        
+        const plnElement = document.querySelector(".js-pln");
+        const currencyElement = document.querySelector(".js-currency");
+        const resultElement = document.querySelector(".js-result");
+
+        const pln = plnElement.value;
+        const currency = currencyElement.value;
+
+        let result = calculateResult(pln, currency);
+
+        resultElement.innerText = `${result.toFixed(2)} ${currency}`
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener("submit", (onFormSubmit));
+
+    };
+
+    init();
+
+}
